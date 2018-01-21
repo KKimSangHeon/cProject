91ec0ad57a633ff8807013bbc50b2dfb5ae58875
KKimsangheon <tkdgjs1501@nate.com>
2018-01-21 23:10:31 +0900

    MainActivity 탭 추가
    탭 클릭 및 동작기능 추가

---
+++ app/src/main/java/kkimsangheon/virtualtradecoin/FragmentTab.java
@@ -0,0 +1,27 @@
+package kkimsangheon.virtualtradecoin;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+/**
+ * Created by SangHeon on 2018-01-21.
+ */
+
+public class FragmentTab extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_layout, container, false);
+        TextView tv = (TextView) v.findViewById(R.id.text);
+        tv.setText(this.getTag() + " Content");
+        return v;
+    }
+}
--- app/src/main/java/kkimsangheon/virtualtradecoin/MainActivity.java
+++ app/src/main/java/kkimsangheon/virtualtradecoin/MainActivity.java
@@ -1,13 +1,41 @@
 package kkimsangheon.virtualtradecoin;
-
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTabHost;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+    private FragmentTabHost mTabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        mTabHost = (FragmentTabHost) findViewById(R.id.tabhost);
+        mTabHost.setup(this, getSupportFragmentManager(), R.id.tabFrameLayout);
+
+        mTabHost.addTab(
+                mTabHost.newTabSpec("coinone").setIndicator(getTabIndicator(mTabHost.getContext(), R.string.tab_title_1, android.R.drawable.star_on)),
+                FragmentTab.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab2").setIndicator(getTabIndicator(mTabHost.getContext(), R.string.tab_title_1, android.R.drawable.star_on)),
+                FragmentTab.class, null);
+        mTabHost.addTab(
+                mTabHost.newTabSpec("tab3").setIndicator(getTabIndicator(mTabHost.getContext(), R.string.tab_title_1, android.R.drawable.star_on)),
+                FragmentTab.class, null);
+    }
+
+    private View getTabIndicator(Context context, int title, int icon) {
+        View view = LayoutInflater.from(context).inflate(R.layout.tab_layout, null);
+        ImageView iv = (ImageView) view.findViewById(R.id.imageView);
+        iv.setImageResource(icon);
+        TextView tv = (TextView) view.findViewById(R.id.textView);
+        tv.setText(title);
+        return view;
     }
 }
